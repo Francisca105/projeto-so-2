f104aaf82e512cdece6cae824df1a2d6ccfe67a6
Francisca105 <franciscaalmeida023@gmail.com>
2023-12-20 15:59:34 +0000

    Aula prática
    Co-authored-by: Virtual <sVirtual@users.noreply.github.com>

--- server/main.c
+++ server/main.c
@@ -1,12 +1,18 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
 
 #include "common/constants.h"
 #include "common/io.h"
 #include "operations.h"
 
+int S = 1; // TODO: session counter
+
 int main(int argc, char* argv[]) {
+  pthread_t main_thread;
+
   if (argc < 2 || argc > 3) {
     fprintf(stderr, "Usage: %s\n <pipe_path> [delay]\n", argv[0]);
     return 1;
@@ -32,8 +38,29 @@ int main(int argc, char* argv[]) {
 
   //TODO: Intialize server, create worker threads
 
+  // Garanties that the pipe is not associated with any other process
+  unlink(argv[1]);
+  
+  // Creates the pipe
+  if (mkfifo(argv[1], 0777)) {
+    fprintf(stderr, "Failed to create pipe\n");
+    return 1;
+  }
+
+  // Creates safe thread
+  if (pthread_create(main_thread,NULL, NULL,NULL) == -1) {
+    fprintf(stderr, "Failed to create main thread\n");
+    return 1;
+  }
+
   while (1) {
     //TODO: Read from pipe
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+      fprintf(stderr, "Failed to open pipe\n");
+      return 1;
+    }
+    
     //TODO: Write new client to the producer-consumer buffer
   }
 
